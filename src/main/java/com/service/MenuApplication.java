e1aa61ed92a3aa498ba0e4264d6a82756cb33590
Abhishek <abhishek.sawant@seligentconsulting.com>
2023-03-30 23:44:51 +0530

    Package name changed

--- src/main/java/com/service/MenuApplication.java
+++ src/main/java/com/service/MenuApplication.java
@@ -1,4 +1,4 @@
-package com.service.menu;
+package com.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/service/ServletInitializer.java
+++ src/main/java/com/service/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.service.menu;
+package com.service;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
