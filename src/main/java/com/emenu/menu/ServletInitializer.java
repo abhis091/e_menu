80615c99c6f29d68103f91c0da6789165868b9cf
Abhishek <abhishek.sawant@seligentconsulting.com>
2023-03-31 00:41:55 +0530

    Project top level structure (packages) added

--- src/main/java/com/emenu/menu/ServletInitializer.java
+++ src/main/java/com/emenu/menu/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.service;
+package com.emenu.menu;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
