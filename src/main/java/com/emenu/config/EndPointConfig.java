c518085881b19062ea754740eecff76716ba4245
Sarthak Shinde <shindesarthak10@gmail.com>
2023-03-31 20:06:53 +0530

    Sarthaks commit

---
+++ src/main/java/com/emenu/config/EndPointConfig.java
@@ -0,0 +1,20 @@
+package com.emenu.config;
+
+public class EndPointConfig {
+	
+	
+	public static final String VERSION_1 = "/v1";
+	
+	public static final String API = "/api";
+	
+	
+	
+	public static final String LOGIN = "/login";
+	
+	public static final String REGISTER = "/register";
+	
+	public static final String MENU = "/menu";
+	
+	public static final String IMAGES = "/images";
+
+}
